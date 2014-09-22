bb9fcba01c2697093ad6db0defd3e45c4bb3f1d6
carlos4ndre <carlos.ulrich@gmail.com>
2014-09-22 22:51:37 +0200

    Added AngularJs routing for register and chatroom pages.

--- web/config.js
+++ web/config.js
@@ -5,17 +5,42 @@ requirejs.config({
         app: '../scripts',
         angular: 'scripts/libs/angular/angular',
         bootstrap: 'scripts/libs/bootstrap/dist/js/bootstrap',
-        'font-awesome': 'scripts/libs/font-awesome/fonts/*',
         requirejs: 'scripts/libs/requirejs/require',
         spin: 'scripts/libs/spin/javascripts/jquery.spin',
+        peerjs: 'scripts/libs/peerjs/peer.min',
+        jquery: 'scripts/libs/jquery/dist/jquery.min',
+        'angular-route': 'scripts/libs/angular-route/angular-route',
+        'angular-resource': 'scripts/libs/angular-resource/angular-resource',
         fontawesome: 'scripts/libs/fontawesome/fonts/*',
-        peerjs: 'scripts/libs/peerjs/peer.min'
+        domready: 'scripts/libs/domready/ready'
     },
+    shim: {
+        bootstrap: [
+            'jquery'
+        ],
+        angular: {
+            exports: 'angular'
+        },
+        'angular-route': {
+            exports: 'ngRoute',
+            deps: [
+                'angular'
+            ]
+        },
+        'angular-resource': {
+            exports: 'ngResource',
+            deps: [
+                'angular'
+            ]
+        }
+    },
+    waitSeconds: 30,
     packages: [
 
-    ],
-    waitSeconds: 30
+    ]
 });
 
 // Load modules
-requirejs(['peerjs','app/main']);
+requirejs(['jquery','domready','bootstrap','angular','angular-route','angular-resource'],function() {
+    requirejs(['app/main']);
+});
--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,4 +1,32 @@
-define(function (require) {
+define(['angular'], function (angular) {
 	console.log("Woooohoooooooo!!!");
-	var peer = new Peer('amazingid', {host: 'localhost', port: 9000});
+
+	//Define an angular module for our app
+	var app = angular.module('phoneboothApp', ['ngRoute']);
+	app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
+		$routeProvider.
+      		when('/', {
+        		templateUrl: 'templates/chatroom.html',
+        		controller: 'chatroomController'
+      		}).
+      		when('/register', {
+        		templateUrl: 'templates/register.html',
+        		controller: 'registerController'
+    		}).
+      		otherwise({
+        		redirectTo: '/register'
+      		});
+	}]);
+
+	app.controller('registerController', function($scope) {
+    	$scope.message = 'This is registerController';
+	});
+
+	app.controller('chatroomController', function($scope) {
+    	$scope.message = 'This is chatroomController';
+	});
+
+    require(['domready'], function (document) {
+        angular.bootstrap(document, ['phoneboothApp']);
+    });
 });
